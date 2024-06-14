1b47110dea28d32fceb23d78b05136eba0fcf971
NayhtSirc <leandrofpires1@gmail.com>
2024-06-14 20:07:19 -0300

    Update alterar.py

--- alterar.py
+++ alterar.py
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-def alterar ():
-    ind = int(input('selecione qual item modificar: '))
-
-    opcao = str(input('deseja alterar o nome(n) ou o valor(v)? '))
-    if opcao == 'n':
-        item_novo = str(input('digite o novo nome: '))
-        itens[ind] = item_novo
-    elif opcao == 'v':
-        valor_novo = float(input('digite o novo valor: '))
-        valor[ind] = valor_novo
-    else:
-        print('digite uma opção válida')
-    print('valor alterado')
-=======
 import json
 from salvar import salvararquivo
 def nomevariavel(var): # retorna o nome da variável
